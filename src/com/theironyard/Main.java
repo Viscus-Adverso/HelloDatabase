d1c8f556f244db8ff1c0c2e2473081376009af26
Viscus <keegansaint194@hotmail.com>
2016-10-12 10:39:46 -0400

    initial push

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -2,11 +2,21 @@ package com.theironyard;
 
 import org.h2.tools.Server;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         Server.createWebServer().start();
+
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS players (id IDENTITY, name VARCHAR, score INT, health DOUBLE, is_alive BOOLEAN)");
+        stmt.execute("INSERT INTO players VALUES (NULL, 'Alice', 10, 100, true)");
+        stmt.execute("UPDATE players SET health = 50 WHERE name = 'Alice'");
+        stmt.execute("DELETE FROM players WHERE name = 'Alice'");
     }
 }
